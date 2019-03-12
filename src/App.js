998f540ac415034c6ea9a500b48a3fbd1bffb5c4
Giacomo Zinetti <giacomo.zinetti@giko.it>
2019-03-12 16:41:31 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 
 class App extends Component {
   render() {
@@ -19,6 +32,28 @@ class App extends Component {
           >
             Learn React
           </a>
+
+          <Router>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about/">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/users/">Users</Link>
+                  </li>
+                </ul>
+              </nav>
+
+              <Route path="/" exact component={Index} />
+              <Route path="/about/" component={About} />
+              <Route path="/users/" component={Users} />
+            </div>
+          </Router>
         </header>
       </div>
     );
